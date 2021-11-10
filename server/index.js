e442f86b5b6437dd864f48f39d5a90a2e701e153
bensandison <56243297+bensandison@users.noreply.github.com>
2021-11-10 12:38:10 +0000

    commenting

--- server/index.js
+++ server/index.js
@@ -9,7 +9,8 @@ const app = express();
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
-  
+
+// http://localhost:3001/api will return a message from the server
 app.listen(PORT, () => {
   console.log("Server running on http://localhost:" + PORT)
 });
