3535d26d8d8dbbf9ee070053c9d44e6ff173d90f
bensandison <56243297+bensandison@users.noreply.github.com>
2021-11-10 14:21:53 +0000

    added react login form

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,12 +1,34 @@
 function Login() {
+  function submitLogin(e) {}
+
   return (
     <div style={{ padding: 20 }}>
       <h2>Login:</h2>
-      <form>
-        <input placeholder="username" type="text"></input>
-        <input placeholder="password" type="text"></input>
-        <input type="submit"></input>
-      </form>
+      <div className="box">
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            className="login-input"
+            placeholder="Username"
+          />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="login-input"
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="button" className="login-btn" onClick={submitLogin}>
+          Login
+        </button>
+      </div>
     </div>
   );
 }
