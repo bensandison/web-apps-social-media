d9ffbc26a7d00a75e84820bd22590dbb41758ca8
bensandison <56243297+bensandison@users.noreply.github.com>
2021-11-21 06:01:43 +0000

    added final REST endpoints

--- server/modules/users.js
+++ server/modules/users.js
@@ -3,11 +3,11 @@ var md5 = require("md5");
 
 function users(app) {
 	//Get a list of users:
+	//TODO: Remove passwords from here
 	app.get("/api/users", (req, res) => {
 		var sql = "select * from user";
 		var params = [];
 		db.all(sql, params, (err, rows) => {
-			console.log(rows);
 			if (err) {
 				res.status(400).json({ error: err.message });
 				return;
@@ -20,7 +20,7 @@ function users(app) {
 	});
 
 	//Get single user by ID:
-	app.get("/api/user/:id", (req, res) => {
+	app.get("/api/users/:id", (req, res) => {
 		var sql = "select * from user where id = ?";
 		var params = [req.params.id]; //ID is a special Express.js endpoint with a variable expression
 		// E.g: a request using /api/user/1 will filter the query using id = 1
@@ -37,10 +37,11 @@ function users(app) {
 	});
 
 	// Create New User:
-	app.post("/api/user/", (req, res) => {
+	app.post("/api/users/", (req, res) => {
 		var errors = [];
 		// Email and Password are required
 		if (!req.body.password) {
+			// body-parser converts req.body to js object
 			errors.push("No password specified");
 		}
 		if (!req.body.email) {
@@ -59,6 +60,7 @@ function users(app) {
 		var sql = "INSERT INTO user (name, email, password) VALUES (?,?,?)";
 		var params = [data.name, data.email, data.password];
 		db.run(sql, params, function (err, result) {
+			//need to use ES5 function so we can access "this.lastID"
 			if (err) {
 				res.status(400).json({ error: err.message });
 				return;
@@ -66,9 +68,58 @@ function users(app) {
 			res.json({
 				message: "success",
 				data: data,
-				id: this.lastID,
+				id: this.lastID, //returning the id means they can retrive the user after creating it
 			});
 		});
+	});
+
+	//Update a user:
+	//TODO: add more security for this - users should not be able to update pw and email together
+	app.patch("/api/users/:id", (req, res) => {
+		var data = {
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password ? md5(req.body.password) : null, //hash pw if it was sent
+		};
+		db.run(
+			// COALESENCE function returns the first argument that is not null
+			`UPDATE user set 
+				name = COALESCE(?,name), 
+				email = COALESCE(?,email), 
+				password = COALESCE(?,password) 
+				WHERE id = ?`,
+			[data.name, data.email, data.password, req.params.id],
+			function (err, result) {
+				if (err) {
+					res.status(400).json({ error: res.message });
+					return;
+				}
+				res.json({
+					message: "success",
+					data: data,
+					changes: this.changes, //number of rows updated (can be used for verification)
+				});
+			}
+		);
+	});
+
+	// Delete a user:
+	//Takes a user ID to be deleted
+	app.delete("/api/user/:id", (req, res, next) => {
+		db.run(
+			"DELETE FROM user WHERE id = ?",
+			req.params.id,
+			function (err, result) {
+				if (err) {
+					res.status(400).json({ error: res.message });
+					return;
+				}
+				res.json({
+					message: "deleted",
+					changes: this.changes, //If the user was already deleted, or the id was not found, the value will be 0
+				});
+			}
+		);
 	});
 }
 
