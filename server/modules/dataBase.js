b339899968e6b66e158ad47689bdf1b3e4529d1f
bensandison <56243297+bensandison@users.noreply.github.com>
2021-12-01 06:31:52 +0000

    removed create table from db file

--- server/modules/dataBase.js
+++ server/modules/dataBase.js
@@ -10,18 +10,7 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
 		console.error(err.message);
 		throw err;
 	} else {
-		db.run(
-			`CREATE TABLE user (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name text, 
-            email text UNIQUE, 
-            password text, 
-            CONSTRAINT email_unique UNIQUE (email)
-            )`,
-			(err) => {
-				if (!err) console.log("Table Created!");
-			}
-		);
+		console.log("database opened");
 	}
 });
 
