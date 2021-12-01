edb52a33387a8a0dac51b783daa3eeb17dae27b6
bensandison <56243297+bensandison@users.noreply.github.com>
2021-12-01 13:09:31 +0000

    added API for posts

---
+++ server/modules/posts.js
@@ -0,0 +1,39 @@
+const db = require("./dataBase")
+
+function createPost(req, res) {
+    let errors = [];
+    //get data
+    let data = {
+        "id" : [req.params.id],
+        "title" : req.body.title,
+        "body" : req.body.body,
+    }
+
+	// ID, title and body are required
+	if (!data.id) {
+		errors.push("No user ID specified");
+	}
+	if (!data.title) {
+		errors.push("No post title specified");
+	}
+    if (!data.body){
+        errors.push("No post body specified")
+    }
+
+    const sql = "INSERT INTO posts (user_id, title, body) VALUES (?,?,?)";
+		const params = [data.id, data.title, data.body];
+		db.run(sql, params, function (err, result) {
+			//need to use ES5 function so we can access "this.lastID"
+			if (err) {
+                console.log(err);
+				res.status(400).json({ error: err.message });
+				return;
+			}
+			res.json({
+				message: "post success",
+				data: data,
+			});
+		});
+}
+
+module.exports = { createPost };
--- server/routes.js
+++ server/routes.js
@@ -6,18 +6,22 @@ const {
 	deleteUser,
 } = require("./modules/users");
 const { createSession } = require("./modules/session");
+const {createPost} = require("./modules/posts");
 
 const express = require("express");
 const router = express.Router();
 
 //Users Route
 router.get("/users", getUsers);
-router.get("/users/id", getUserById);
+router.get("/users/:id", getUserById);
 router.post("/users", createUser);
 router.patch("/users", updateUser);
 router.delete("/users", deleteUser);
 
 //Session Route
 router.post("/session", createSession);
+
+//Posts Route
+router.post("/posts/:id", createPost);
 
 module.exports = router;
