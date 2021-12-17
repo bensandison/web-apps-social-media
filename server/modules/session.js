40118de97b050c1ad2477d0543ddf04ce48c8995
bensandison <56243297+bensandison@users.noreply.github.com>
2021-12-17 03:47:50 +0000

    working tokens - no password checking currently

--- server/modules/session.js
+++ server/modules/session.js
@@ -1,30 +1,25 @@
 const bcrypt = require("bcrypt");
 const db = require("./database.js");
 const UUID = require("uuid");
+const e = require("express");
 
-function createSession(req, res) {
-	let result = false; //stores new sql row including token
+function createSession(req, res, next) {
 	const sqlGet = "SELECT * FROM user WHERE email = ?";
 	db.get(sqlGet, [req.body.email], function (err, row) {
-		if (row) result = row; //set result to row if it was returned properley
+		if (!row) next("Incorrect username or password");
 
 		if (row && !row.token) {
-			// Check uses dosent already have a token
-			// Create an API token for the user:
+			// Create an API token for the user if they dont already have one:
 			let token = UUID.v4();
 
 			//Store token in user db
 			const sqlRun = "UPDATE user SET token = ? WHERE id = ?";
 			db.run(sqlRun, [token, row.id], function () {
-				result.token = token;
-				res.json(result);
+				row.token = token; //add token to user data
+				res.json(row); //respond with user data including new token
 			});
-		} else {
-			res.json(result);
-		}
-	})
+		} else res.json(row); //respond with user data
+	});
 }
-
-//TODO: add catch for errors
 
 module.exports = { createSession };
