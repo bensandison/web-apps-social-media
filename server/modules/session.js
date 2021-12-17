faa7b806a2c598e7dd2c419d33d7c301457403d8
bensandison <56243297+bensandison@users.noreply.github.com>
2021-12-17 04:12:32 +0000

    added password checking to createSession

--- server/modules/session.js
+++ server/modules/session.js
@@ -1,24 +1,29 @@
 const bcrypt = require("bcrypt");
 const db = require("./database.js");
 const UUID = require("uuid");
-const e = require("express");
 
 function createSession(req, res, next) {
 	const sqlGet = "SELECT * FROM user WHERE email = ?";
 	db.get(sqlGet, [req.body.email], function (err, row) {
-		if (!row) next("Incorrect username or password");
+		if (!row) next("Incorrect username or password"); //Incorrect User Name
 
-		if (row && !row.token) {
-			// Create an API token for the user if they dont already have one:
-			let token = UUID.v4();
+		//compare user submited password against database
+		bcrypt.compare(req.body.password, row.password, function (err, result) {
+			if (!result) next("Incorrect username or password"); //Incorrect Password
 
-			//Store token in user db
-			const sqlRun = "UPDATE user SET token = ? WHERE id = ?";
-			db.run(sqlRun, [token, row.id], function () {
-				row.token = token; //add token to user data
-				res.json(row); //respond with user data including new token
-			});
-		} else res.json(row); //respond with user data
+			if (row && !row.token) {
+				//if user has no token in DB
+				// Create an API token for the user if they dont already have one:
+				let token = UUID.v4();
+
+				//Store token in user db
+				const sqlRun = "UPDATE user SET token = ? WHERE id = ?";
+				db.run(sqlRun, [token, row.id], function () {
+					row.token = token; //add token to user data
+					res.json(row); //respond with user data including new token
+				});
+			} else res.json(row); //respond with user data
+		});
 	});
 }
 
