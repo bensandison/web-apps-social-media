32283a0f06b9a7ed5697770b5a8279590150e805
bensandison <sandisonben@gmail.com>
2021-12-22 04:49:12 +0000

    added error handling to createSession

--- server/modules/session.js
+++ server/modules/session.js
@@ -3,34 +3,41 @@ const db = require("./database.js");
 const UUID = require("uuid");
 
 function createSession(req, res, next) {
-	const sqlGet = "SELECT * FROM user WHERE email = ?";
-	db.get(sqlGet, [req.body.email], function (err, row) {
-		if (!row) next("Incorrect username or password"); //Incorrect User Name
+	db.get(
+		"SELECT * FROM user WHERE email = ?",
+		[req.body.email],
+		function (err, row) {
+			if (err) return next(err);
+			if (!row) return next(new Error("Incorrect username or password")); //Incorrect User Name
 
-		//compare user submited password against database
-		bcrypt.compare(req.body.password, row.password, function (err, result) {
-			if (!result) next("Incorrect username or password"); //Incorrect Password
+			//compare user submited password against database
+			bcrypt.compare(req.body.password, row.password, function (err, result) {
+				if (err) return next(err); //bcrypt.compare error
+				if (!result) next(new Error("Incorrect username or password")); //Incorrect Password
 
-			if (row && !row.token) {
-				//if user has no token in DB
-				// Create an API token for the user if they dont already have one:
-				let token = UUID.v4();
+				if (row && !row.token) {
+					//if user has no token in DB
+					// Create an API token for the user if they dont already have one:
+					let token = UUID.v4();
 
-				//Store token in user db
-				const sqlRun = "UPDATE user SET token = ? WHERE id = ?";
-				db.run(sqlRun, [token, row.id], function () {
-					row.token = token; //add token to user data
-					res.json(row); //respond with user data including new token
-				});
-			} else res.json(row); //respond with user data
-		});
-	});
+					//Store token in user db
+					const sqlRun = "UPDATE user SET token = ? WHERE id = ?";
+					db.run(sqlRun, [token, row.id], function (err) {
+						if (err) return next(err);
+
+						row.token = token; //add token to user data
+						res.json(row); //respond with user data including new token
+					});
+				} else res.json(row); //respond with user data
+			});
+		}
+	);
 }
 
 //Get User Data from Token
 function findByToken(req, res, next) {
 	db.get(
-		"SELECT * FROM use WHERE token = ?",
+		"SELECT * FROM user WHERE token = ?",
 		req.body.token,
 		function (err, row) {
 			if (err) return next(err); //database.get fails
