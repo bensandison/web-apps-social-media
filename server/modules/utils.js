3d64e8c4ec913e228168917c35052cd6a8a12424
bensandison <sandisonben@gmail.com>
2021-12-29 06:27:02 +0000

    commenting

--- server/modules/utils.js
+++ server/modules/utils.js
@@ -1,9 +1,11 @@
 const db = require("./database.js");
 
+// takes a token and passes user data to the callback fn
 function findByToken(token, callback) {
 	db.get("SELECT * FROM user WHERE token = ?", token, function (err, row) {
-		if (err) return callback(err); //database.get fails
-		//for empty user data:
+		// if db.get fails
+		if (err) return callback(err);
+		// for empty user data:
 		if (!row) return callback(new Error("No user data for this token"));
 		// respond with user data
 		else return callback(null, row);
