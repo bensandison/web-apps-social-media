13d49089bcf6114b126aa77cd543da8a7e742c18
bensandison <sandisonben@gmail.com>
2021-12-29 08:08:49 +0000

    fixed updateUser api route

--- server/routes.js
+++ server/routes.js
@@ -15,7 +15,7 @@ const router = express.Router();
 router.get("/users", getUsers);
 router.get("/users/:id", getUserById);
 router.post("/users", createUser);
-router.patch("/users", updateUser);
+router.patch("/users/:id", updateUser);
 router.delete("/users/:id", deleteUser);
 
 //Session Route
