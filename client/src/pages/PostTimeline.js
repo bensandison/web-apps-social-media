54e2fa78f252529256dd6dd7eedf6015fd5f96d2
bensandison <56243297+bensandison@users.noreply.github.com>
2022-01-17 01:04:37 +0000

    working post timeline

--- client/src/pages/PostTimeline.js
+++ client/src/pages/PostTimeline.js
@@ -1,15 +1,38 @@
 // import Axios from "axios";
-import { Typography, Container } from "@mui/material";
-import { useState } from "react";
+import { Typography, Container, Card } from "@mui/material";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function PostTimeline() {
-	const [posts, setPosts] = useState();
+	const [postData, setPostData] = useState();
+
+	useEffect(() => {
+		Axios.get("/api/posts/all").then(function (response) {
+			setPostData(response.data.data);
+			console.log("hi");
+		});
+	}, []);
+
+	const postComponents = postData?.map((data, index) => (
+		<Post data={data} key={index}></Post>
+	));
 
 	return (
 		<Container maxWidth="xs" padding={2}>
 			<Typography variant="h3">Post Timeline</Typography>
-			<Typography>hi</Typography>
+			{postComponents}
 		</Container>
+	);
+}
+
+function Post(props) {
+	const data = props.data;
+	return (
+		<Card>
+			<Typography variant="subtitle">{data.user_name}</Typography>
+			<Typography variant="h4">{data.title}</Typography>
+			<Typography variant="body1">{data.body}</Typography>
+		</Card>
 	);
 }
 
