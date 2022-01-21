f2d427a365655aaa9fb3a8c47ae5d09d19bf7913
bensandison <sandisonben@gmail.com>
2022-01-21 19:40:59 +0000

    added like and comment icons

--- client/src/pages/PostTimeline.js
+++ client/src/pages/PostTimeline.js
@@ -1,6 +1,12 @@
 // import Axios from "axios";
-import { Typography, Container, Card, Stack } from "@mui/material";
+import { Typography, Container, Card, Stack, Button } from "@mui/material";
 import { useState, useEffect } from "react";
+import {
+	Favorite,
+	FavoriteBorder,
+	ModeComment,
+	ModeCommentOutlined,
+} from "@mui/icons-material";
 import Axios from "axios";
 
 function PostTimeline() {
@@ -19,7 +25,7 @@ function PostTimeline() {
 
 	return (
 		<Container maxWidth="sm">
-			<Stack stack={2}>
+			<Stack spacing={2}>
 				<Typography variant="h4">Post Timeline</Typography>
 				{postData?.map((data, index) => (
 					<Post data={data} key={index}></Post>
@@ -30,20 +36,48 @@ function PostTimeline() {
 }
 
 function Post(props) {
+	// Has this user liked or commented?
+	// TODO: set these from DB
+	const [liked, setLiked] = useState(false);
+	const [commented, setCommented] = useState(false);
+
 	const data = props.data;
 	return (
-		<Card sx={{ p: 1, my: 1 }}>
-			<Typography variant="subtitle">{data.user_name}</Typography>
-			<Typography variant="h5">{data.title}</Typography>
-			<Typography variant="body1">{data.body}</Typography>
-			{data.image_name && (
-				//if image provided:
-				<img
-					src={"/uploads/" + data.image_name}
-					alt={data.title}
-					height="200"
-				/>
-			)}
+		<Card sx={{ p: 2 }}>
+			<Stack spacing={1}>
+				<Typography variant="subtitle">{data.user_name}</Typography>
+				<Typography variant="h5">{data.title}</Typography>
+				<Typography variant="body1">{data.body}</Typography>
+				{data.image_name && (
+					//if image provided:
+					<img
+						src={"/uploads/" + data.image_name}
+						alt={data.title}
+						height="200"
+					/>
+				)}
+				<Container>
+					<Button
+						onClick={function () {
+							//add/remove comment from db
+							setLiked(!liked); //TODO: use callback here
+						}}
+					>
+						{liked ? <Favorite></Favorite> : <FavoriteBorder></FavoriteBorder>}
+					</Button>
+					<Button
+						onClick={function () {
+							//add/remove comment from db
+						}}
+					>
+						{commented ? (
+							<ModeComment></ModeComment>
+						) : (
+							<ModeCommentOutlined></ModeCommentOutlined>
+						)}
+					</Button>
+				</Container>
+			</Stack>
 		</Card>
 	);
 }
