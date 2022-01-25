b0c8c5eaf007411709d3a0c88ea536efeea312fe
bensandison <56243297+bensandison@users.noreply.github.com>
2022-01-25 00:12:29 +0000

    comment count updates

--- client/src/components/CommentButton.js
+++ client/src/components/CommentButton.js
@@ -1,19 +1,31 @@
-import { Button } from "@mui/material";
-import { ModeComment, ModeCommentOutlined } from "@mui/icons-material";
+import { Button, Typography } from "@mui/material";
+import { ModeCommentOutlined } from "@mui/icons-material";
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Axios from "axios";
+import axiosError from "../utils/axiosError";
 
 export default function CommentButton({ data }) {
-	// COMMENTS:
-	const [commented, setCommented] = useState(false);
-	const [commentCount, setCommentCount] = useState(null);
+	//runs on first render
+	useEffect(() => {
+		getCommentCount();
+	}, []);
+
+	const [commentCount, setCommentCount] = useState(0);
+	function getCommentCount() {
+		Axios.get("/api/comments/count" + data.post_index)
+			.then((response) => {
+				if (!response.data.commentCount) response.data.commentCount = 0;
+				setCommentCount(response.data.commentCount);
+			})
+			.catch((error) => {
+				axiosError(error);
+			});
+	}
 	return (
 		<Button to="/post" component={Link} state={{ data }}>
-			{commented ? (
-				<ModeComment></ModeComment>
-			) : (
-				<ModeCommentOutlined></ModeCommentOutlined>
-			)}
+			<ModeCommentOutlined></ModeCommentOutlined>
+			<Typography>{commentCount}</Typography>
 		</Button>
 	);
 }
