4b3cc40271f431ef665f297f0c11250fd59ffe8e
bensandison <sandisonben@gmail.com>
2022-01-25 15:14:38 +0000

    added welcome page and default route tocheck login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "@mui/material";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
+import WelcomePage from "./pages/WelcomePage";
 import LogInForm from "./pages/LogInForm";
 import SignUpForm from "./pages/SignUpForm";
 import CreatePost from "./pages/CreatePost";
@@ -22,6 +23,8 @@ function App() {
 				</Container>
 				<Container component="main" sx={{ pt: 2 }}>
 					<Routes>
+						<Route path="/"></Route>
+						<Route path="/welcome" element={<WelcomePage />} />
 						<Route path="/signup" element={<SignUpForm />} />
 						<Route path="/login" element={<LogInForm />} />
 						<Route path="/createpost" element={<CreatePost />} />
---
+++ client/src/pages/WelcomePage.js
@@ -0,0 +1,11 @@
+import { Container, Stack, Typography, Button } from "@mui/material";
+
+export default function WelcomePage() {
+	return (
+		<Container>
+			<Stack spacing={2}>
+				<Typography variant="h4">Welcome:</Typography>
+			</Stack>
+		</Container>
+	);
+}
