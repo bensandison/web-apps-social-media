003ace27fd3cbf7ffb9c3ebb83d118f47910ef34
bensandison <sandisonben@gmail.com>
2022-01-29 23:01:04 +0000

    add mounted check

--- client/src/components/CommentButton.js
+++ client/src/components/CommentButton.js
@@ -1,11 +1,20 @@
 import { Button, Typography } from "@mui/material";
 import { ModeCommentOutlined } from "@mui/icons-material";
 import { Link } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Axios from "axios";
 import axiosError from "../utils/axiosError";
 
 export default function CommentButton({ data }) {
+	// Setting state when component has been unmounted can cause errors:
+	const isMounted = useRef(false);
+	useEffect(() => {
+		isMounted.current = true;
+		return () => {
+			isMounted.current = false;
+		};
+	}, []);
+
 	//runs on first render
 	useEffect(() => {
 		getCommentCount();
@@ -15,7 +24,7 @@ export default function CommentButton({ data }) {
 	function getCommentCount() {
 		Axios.get("/api/comments/count/" + data.post_index)
 			.then((response) => {
-				setCommentCount(response.data.commentCount);
+				if (isMounted.current) setCommentCount(response.data.commentCount);
 			})
 			.catch((error) => {
 				axiosError(error);
